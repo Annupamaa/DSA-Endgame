2ca36f604ad63aaf75e3c7a123460c0b88beb805
Annupamaa <annu242005@gmail.com>
2024-02-19 09:15:22 +0530

    DAY10

---
+++ Day1-10/day10.java
@@ -0,0 +1,23 @@
+class Solution{
+    public int[][] merge(int[][] intervals){
+        ArrayList<int[]> result = new ArrayList<>();
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
+
+        for(int i=1; i<intervals.length; i++){
+            if(intervals[0][1] >= intervals[i][0]){
+                intervals[0][1] = Math.max(intervals[0][1], intervals[i][1]);
+            }else {
+                result.add(intervals[0]);
+                intervals[0] = intervals[i];
+            }
+        }
+        
+        result.add(intervals[0]);
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+
+
+
